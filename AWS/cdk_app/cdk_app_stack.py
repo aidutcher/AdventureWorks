1d6401c752dc94b73c82debcf94f86516f0851e8
Alec Dutcher <77470743+aidutcher@users.noreply.github.com>
2022-03-21 19:29:50 -0500

    Configured test, dev, and prod buckets
    Blocked public access on each bucket

--- AWS/cdk_app/cdk_app_stack.py
+++ AWS/cdk_app/cdk_app_stack.py
@@ -7,10 +7,23 @@ class CdkAppStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-
-        bucket = s3.Bucket(self, "test-aw-database-snapshots", 
+        # Defining the test, dev, and prod database snapshot buckets
+        # Standup these buckets, test pipeline features, then destroy
+        
+        test_bucket = s3.Bucket(self, "test-aw-database-snapshots", 
             versioned=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=cdk.RemovalPolicy.DESTROY,
             auto_delete_objects=True)
                 
+        dev_bucket = s3.Bucket(self, "dev-aw-database-snapshots", 
+            versioned=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL, 
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True)
             
+        prod_bucket = s3.Bucket(self, "prod-aw-database-snapshots", 
+            versioned=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,  
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True)
